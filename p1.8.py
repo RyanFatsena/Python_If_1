2a5baa802497996e1a415e7ae9ec278e6866ae0d
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-10-21 23:27:54 +0700

    Create p1.8.py

---
+++ p1.8.py
@@ -0,0 +1,18 @@
+#13
+a = -2
+b = 9
+if (a > 0) and (b> 0):
+    print("Keduanya positif")
+else:
+    print("Keduanya tidak positif")
+    
+a = -2
+b = 9
+if (a > 0) :
+    if (b > 0) :
+        print("Keduanya positif")
+    else:
+        print("Keduanya tidak positif")
+else:
+    print("Keduanya tidak positif")
+
