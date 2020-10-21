d70a3ae6d855d0fb493ff5c701cdb0450f864b09
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-10-21 22:58:57 +0700

    Create p1.6.py

---
+++ p1.6.py
@@ -0,0 +1,23 @@
+#11
+a = -2
+b = -7
+if (a > 0) and (b > 0):
+    print("Keduanya positif")
+if (a > 0) or (b > 0):
+    print("Salah satunya positif")
+if (a < 0) or (Ь < 0):
+    print("Salah satunya negatif")
+if (a < 0) and (b < 0):
+    print("Keduanya negatif")
+    
+#8
+a = -2
+b = -7
+if (a > 0) and (b > 0):
+    print("Keduanya positif")
+elif (a > 0) or (b > 0):
+    print("Salah satunya positif")
+elif (a < 0) or (Ь < 0):
+    print("Salah satunya negatif")
+elif (a < 0) and (b < 0):
+    print("Keduanya negatif")
