c7689ca369042df610239824e416ebc24eff3bf3
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-10-24 15:59:59 +0700

    Update L2.py

--- L2.py
+++ L2.py
@@ -6,11 +6,11 @@ ipa = int(input('Nilai IPA:'))
 
 print('==========================')
 
-if(indo <= 0 or indo >= 100):
+if(indo < 0 or indo > 100):
     print('Maaf input ada yang tidak valid')
-elif(ipa <= 0 or ipa >= 100):
+elif(ipa < 0 or ipa > 100):
     print('Maaf input ada yang tidak valid')
-elif (mtk <= 0 or mtk >= 100):
+elif (mtk < 0 or mtk > 100):
     print('Maaf input ada yang tidak valid')
     
 elif(indo > 60 and ipa > 60 and mtk > 70):
